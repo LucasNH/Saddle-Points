8e0b710cd97ee1e896340af3f59c011a6683168f
Lucas Noritomi-Hartwig <lucasnoritomi-hartwig@Lucas-MacBook-Pro.local>
2021-12-20 01:09:15 -0500

    Wrote solution in Python and wrote README.md

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # Saddle-Points
 Finding Saddle Points in a Matrix
+
+I do not take credit for this question.
+Here is the link to the video where I first encountered this question:
+https://youtu.be/Wm5eLTeC9KM
+
+The original question:
+
+    University of Waterloo, Faculty of Engineering, Department of Electrical & Computer Engineering
+
+    ECE150 - Fundamentals of Programming, December 2020 Final Exam Question:
+
+    We define a saddle point in a matrix as being an entry of a matrix that is:
+    - Strictly less than any entry in the given row
+    - Strictly greater than any entry in the given column
+
+The goal is to complete given C++ starter code to identify if a given matrix has a saddle point or not.
+If the matrix does have a saddle point, the program should return true, and if it does not, the program shoudl return false.
+
+This question was originally intended for the solution to be in written in C++. However, I have written my own solution to the problem in Python.
+
+In my Python implementation, as soon as the first saddle point is found, the program returns the index of the point and terminates the search.
+This is because there can only be at most one saddle point (as defined in the question) for any given matrix.
+
+
+Proof that there can only exist at most one saddle point in a matrix:
+(The question asked on the exam did not ask for a proof, I am only including it here to help readers understand why the program only checks for one saddle point.)
+
+Assume there exists a matrix A that has two or more saddle points.
+We will only consider two saddle points, a and b, that lie in a matrix. However, this is not a problem as the reasoning in this proof can be reapplied to any number of saddle points.
+
+Let a and b be the two saddle points in matrix A.
+Then we know that a and b must lie on different rows and different columns.
+
+Let x be the point on the same column as a and on the same row as b, and let y be the point on the same column as b and on the same row as a.
+
+Then, W.L.O.G, the following grid is in A:
+
+      .       .
+      .       .
+      .       .
+. . . a . . . y . . .
+      .       .
+      .       .
+      .       .
+. . . x . . . b . . .
+      .       .
+      .       .
+      .       .
+
+By definition, a is the smallest number in its row, so a < y, and a is also the largest number in its column, so a > x.
+The following is true for b for the same reason as above: b < x, b > y.
+
+If we combine the satements for a, we get:
+x < a < y, implying x < y
+
+and if we combine the statements for b, we get:
+y < b < x, implying y < x
+
+This is a contradiction as there exist no numbers x, y such that x < y and y < x.
+
+Therefore, either a or b (not both) can be a saddle point.
+
+As mentioned eariler, this reasoning can be applied again to the next supposed saddle point, and so on.
+
+Therefore, it is proven that no matrix has more than one saddle point (as defined by the question).
---
+++ src/saddle_python.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+
+def main():
+    """
+    This is my solution implemented in Python. In this implementation, as soon as the first saddle
+    point is found, the program returns the index of the point and terminates the search. This is
+    because there can only be at most one saddle point (as defined in the question) for any given
+    matrix.
+
+    For the proof of the above statement, see README.md
+    """
+    # Given matrix
+    input_matrix_1 = np.array(
+        [[22.128, 71.599, 24.083, 79.442, 8.4186, 55.744],
+         [14.83, 67.261, 85.3, 38.613, 59.892, 51.837],
+         [6.6377, 93.302, 52.451, 62.743, 33.464, 34.494],
+         [23.96, 91.573, 86.388, 28.882, 94.461, 53.222],
+         [7.1165, 81.087, 44.558, 13.754, 85.023, 18.426]])
+
+    # Test matrices
+    input_matrix_2 = np.array([[1, 2], [0, 1]])
+    input_matrix_3 = np.array([[1, 2], [1, 1]])
+    input_matrix_4 = np.array([[6, 14, 9], [3, 21, 10]])
+
+    # We set matrix A to whichever input matrix defined above
+    A = input_matrix_1
+
+    print("\nInput matrix A:")
+    print(A)
+
+    sp_index = get_saddle_point(A)
+
+    if not sp_index:
+        print("\nThe matrix A has no saddle points.")
+    else:
+        print("\nThe saddle points of the matrix A are:\nA" + \
+            str(sp_index) + ": " + str(A[sp_index[0], sp_index[1]]))
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        for num in row:
+            print(num, end=" ")
+        print()
+
+
+def get_saddle_point(matrix) -> list:
+    """
+    This function iterates through an m x n matrix and checks if each entry is
+    a saddle point as defined above. If an entry is a saddle point, it is
+    added to a dictionary where the key value pair is the index of the entry and
+    the value of the entry.
+
+    Precondition: <matrix> is a numpy.array
+    Postcondition: returns a dictionary containing all saddle points of
+    <matrix>.
+    """
+    matrix_t = matrix.transpose()
+    m, n = len(matrix), len(matrix[0])
+    for i in range(m):
+        for j in range(n):
+            if exclusive_min(j, matrix[i]) and exclusive_max(i, matrix_t[j]):
+                return [i, j]
+    return None
+
+
+def exclusive_min(index, lst) -> bool:
+    """
+    This function checks if lst[index] is strictly smaller than every other
+    element in lst.
+
+    Precondition: 0 <= index < len(lst) (i.e. <index> is valid for lst)
+    Postcondition: returns True if lst[index] is strictly smaller than every
+    other element in lst, False otherwise.
+    """
+    for i in range(len(lst)):
+        if i != index and lst[i] <= lst[index]:
+            return False
+    return True
+
+
+def exclusive_max(index, lst):
+    """
+    This function checks if lst[index] is strictly greater than every other
+    element in lst.
+
+    Precondition: 0 <= index < len(lst) (i.e. <index> is valid for lst)
+    Postcondition: returns True if lst[index] is strictly greater than every
+    other element in lst, False otherwise.
+    """
+    for i in range(len(lst)):
+        if i != index and lst[i] >= lst[index]:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    main()
